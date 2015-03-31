4f1eed4c4cdc370cc0f4bac6338dfe401ac85cc2
Matthew Shultz <matthew.shultz@yale.edu>
2015-03-31 07:31:21 -0400

    Created a simple exponential decay pool

---
+++ pool.R
@@ -0,0 +1,17 @@
+library(deSolve)
+
+pool =  function(t, y, params) {
+  DOC = y[1]
+  with(as.list(params), {
+    dDOC = -k * DOC
+    return(list(dDOC))
+  })
+}
+
+params = c(k=.1)
+Initial.DOC = 100 # units ?
+t = 1:200
+
+out = ode(y = Initial.DOC, times = t, func = pool, parms = params)
+
+matplot(t, out[,2], type = "l", ylab = "p", xlab = "time")
