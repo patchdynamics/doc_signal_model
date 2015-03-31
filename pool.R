989ece12d59afe38aa23878ede4ba490c5b0c637
Matthew Shultz <matthew.shultz@yale.edu>
2015-03-31 07:41:24 -0400

    Allow for variable DOCin

--- pool.R
+++ pool.R
@@ -1,16 +1,26 @@
 library(deSolve)
 
+DOCin = function(t) {
+  if(t < 80 || t > 120){
+    return(8)
+  } else {
+    return(20)
+  }
+}
+
 pool =  function(t, y, params) {
   DOC = y[1]
   with(as.list(params), {
-    dDOC = -k * DOC + DOCin
+    dDOC = -k * DOC + DOCin(t)
     return(list(dDOC))
   })
 }
 
 params = c(k=.1, DOCin = 8)
 Initial.DOC = 100 # units ?
-t = 1:200
+one.week = 24*7
+two.weeks = 24*14
+t = 1:two.weeks
 
 out = ode(y = Initial.DOC, times = t, func = pool, parms = params)
 
