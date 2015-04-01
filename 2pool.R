b9ccc646d8a1c8d95a11f0037f45451d68c2e3bb
Matthew Shultz <matthew.shultz@yale.edu>
2015-03-31 22:31:38 -0400

    Model with priority eating

--- 2pool.R
+++ 2pool.R
@@ -1,6 +1,8 @@
 library(deSolve)
 range = 5 # range of the lognormal distribution to use
-MaxTotalDOCUptake = 2.42 # g/h per km stream, the maximum rate derived from  McLaughlin and Kaplan 2013
+MaxTotalDOCUptake = 2.42  # g/h per km stream, the maximum rate derived from  McLaughlin and Kaplan 2013
+# this factor of 20 effectively extends the reach by to 20 km
+
 BiofilmBacteriaFraction = .8; # for now set density of bacteria at 80% of their maximum possible
 MaximumPossibleUptake = MaxTotalDOCUptake # same as above, at least for now
 
@@ -12,7 +14,7 @@ DOCin = function(t, subpool, poolDivisions) {
   #print(segment)
   #print(portion)
   
-  DOCinTotal = 317
+  DOCinTotal = 211
   PoolDOCin = DOCinTotal * portion
   return(PoolDOCin)
 }
@@ -27,8 +29,6 @@ k = function(subpool, poolDivisions){
 }
 
 pool =  function(t, y, params) {
-
-  print(t)
   
   BiofilmBacteriaFraction = 1
   MaximumFeasibleUptakeRate = MaximumPossibleUptake  # per hour
@@ -49,13 +49,13 @@ pool =  function(t, y, params) {
       
       #print(UptakeRateThisTimeStep)
       
-      dDOC = DOCin(t, i, PoolDivisions) - UptakeRate * BiofilmBacteriaFraction - DOCoutflow
+      dDOC = DOCin(t, i, PoolDivisions) - UptakeRate *  BiofilmBacteriaFraction - DOCoutflow
       dDOCoutflow = Dout * dDOC
       differentials[index] = dDOC
       differentials[index+1] = dDOCoutflow
 
     }
-    print(differentials)
+    #print(differentials)
     list(differentials)
   })
 }
@@ -69,7 +69,7 @@ pool =  function(t, y, params) {
 # assume the inflow to each pool is equal (this is not true)
 # outflows are equal percentages (this will be true)
 # also assume initial DOC is the same and 0 for both pools
-PoolDivisions = 5
+PoolDivisions = 100
 params = c(Dout = .2, PoolDivisions = PoolDivisions)
 
 Initial.DOC = 0 # units ?
@@ -77,7 +77,7 @@ Initial.DOCoutflow = Initial.DOC * params['Dout']
 one.week = 24*7
 two.weeks = 24*14
 t = 1:two.weeks
-max = 50
+max = one.week
 t = 1:max
 
 
@@ -87,6 +87,7 @@ out = ode(y = state, times = t, func = pool, parms = params)
 par(mfrow=c(2,2))
 matplot(t, out[,2:3], type = "l", ylab = "p", xlab = 'hours')
 matplot(t, out[,4:5], type = "l", ylab = "p", xlab = 'hours')
+#matplot(t, sapply(1:PoolDivisions, function(subpool){out[,1+2*subpool]}))
 
 #par(mfrow=c(1,2))
 inflows = sapply(1:PoolDivisions, function(subpool){DOCin(1, subpool, PoolDivisions)})
