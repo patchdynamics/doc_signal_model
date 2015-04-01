c5d0b6bf4cba9ea40ae3884da1d4b55f98d8a536
Matthew Shultz <matthew.shultz@yale.edu>
2015-03-31 21:21:15 -0400

    Working on food source preference

--- 2pool.R
+++ 2pool.R
@@ -1,6 +1,8 @@
 library(deSolve)
 range = 5 # range of the lognormal distribution to use
 MaxTotalDOCUptake = 2.42 # g/h per km stream, the maximum rate derived from  McLaughlin and Kaplan 2013
+BiofilmBacteriaFraction = .8; # for now set density of bacteria at 80% of their maximum possible
+MaximumPossibleUptake = MaxTotalDOCUptake # same as above, at least for now
 
 DOCin = function(t, subpool, poolDivisions) {
   segment = range / poolDivisions
@@ -26,15 +28,28 @@ k = function(subpool, poolDivisions){
 
 pool =  function(t, y, params) {
 
-  BiofilmDensity = 1;
-    
+  print(t)
+  
+  BiofilmBacteriaFraction = 1
+  MaximumFeasibleUptakeRate = MaximumPossibleUptake  # per hour
+  UptakeRateThisTimeStep = 0 # summation
+  
   with(as.list(params), {
     differentials = c()
     for(i in 1:PoolDivisions){
       index = (i-1)*2 + 1
       DOC = y[index]
       DOCoutflow = y[index+1]
-      dDOC = DOCin(t, i, PoolDivisions) - k(i, PoolDivisions) * DOC * BiofilmDensity - DOCoutflow
+      
+      UptakeRate = k(i, PoolDivisions) * DOC ;
+      if(UptakeRate + UptakeRateThisTimeStep > MaximumFeasibleUptakeRate){
+        UptakeRate = MaximumFeasibleUptakeRate - UptakeRateThisTimeStep
+      }
+      UptakeRateThisTimeStep = UptakeRateThisTimeStep + UptakeRate
+      
+      #print(UptakeRateThisTimeStep)
+      
+      dDOC = DOCin(t, i, PoolDivisions) - UptakeRate * BiofilmBacteriaFraction - DOCoutflow
       dDOCoutflow = Dout * dDOC
       differentials[index] = dDOC
       differentials[index+1] = dDOCoutflow
@@ -54,15 +69,15 @@ pool =  function(t, y, params) {
 # assume the inflow to each pool is equal (this is not true)
 # outflows are equal percentages (this will be true)
 # also assume initial DOC is the same and 0 for both pools
-PoolDivisions = 100
-params = c(k=c(.5,.1), DOCin = 8, Dout = .2, PoolDivisions = PoolDivisions)
+PoolDivisions = 5
+params = c(Dout = .2, PoolDivisions = PoolDivisions)
 
 Initial.DOC = 0 # units ?
 Initial.DOCoutflow = Initial.DOC * params['Dout']
 one.week = 24*7
 two.weeks = 24*14
 t = 1:two.weeks
-max = 40
+max = 50
 t = 1:max
 
 
@@ -70,20 +85,20 @@ state = rep(0, params['PoolDivisions'] * 2)
 out = ode(y = state, times = t, func = pool, parms = params)
 
 par(mfrow=c(2,2))
-matplot(t, out[,2:3], type = "l", ylab = "p", xlab = "time")
-matplot(t, out[,4:5], type = "l", ylab = "p", xlab = "time")
+matplot(t, out[,2:3], type = "l", ylab = "p", xlab = 'hours')
+matplot(t, out[,4:5], type = "l", ylab = "p", xlab = 'hours')
 
 #par(mfrow=c(1,2))
 inflows = sapply(1:PoolDivisions, function(subpool){DOCin(1, subpool, PoolDivisions)})
 outflows = sapply(1:PoolDivisions, function(subpool){out[,1+2*subpool][max]})
 ylimit = max(inflows)
 ks = -sapply(1:PoolDivisions, k, PoolDivisions);
-plot(inflows~ks, type="l", ylim=c(0,ylimit) )
-plot(outflows~ks,type="l", ylim=c(0,ylimit))
+plot(inflows~ks, type="l", ylim=c(0,ylimit), xlab='-k' )
+plot(outflows~ks,type="l", ylim=c(0,ylimit), xlab='-k' )
 
-par(mfrow=c(1,1))
-matplot(t, out[,2:90], 2, type = "l")
+#par(mfrow=c(1,1))
+#matplot(t, out[,2:90], 2, type = "l")
 
 
-par(mfrom=c(2,2))
-t1 = sapply(1:PoolDivisions, function(subpool){out[1+subpool]})
+#par(mfrow=c(2,2))
+#t1 = sapply(1:PoolDivisions, function(subpool){out[1+subpool]})
--- notes.R
+++ notes.R
@@ -1,3 +1,5 @@
 sum(sapply(1:5, function(subpool){ DOCin(1, subpool, 5)  }))
 
 out = sapply(1:5, function(subpool){ DOCin(1, subpool, 5)  })
+
+out = sapply(1:5, function(subpool){ k(subpool, 5)  })
